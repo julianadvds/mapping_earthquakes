b761e2ea799d2b16a5c5638bc49408e32028559d
Juliana dvdS <julianadvds@gmail.com>
2022-02-02 22:49:39 -0500

    completed multiple points mapping

--- mapping_multiple_points/static/js/logic.js
+++ mapping_multiple_points/static/js/logic.js
@@ -1,24 +1,9 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
+
 // Create the map object with a centre and zoom level
 let map = L.map('mapid').setView([40.7,-94.5],4);
-
-// add markers to map for LA
-// L.circle([34.0522, -118.2437], {
-//     radius: 300,
-//     color: 'black',
-//     fillColor: 'lightyellow',
-//     fillOpacity: 0.25
-    
-//  }).addTo(map);
-
-// Add a Circle Marker
-L.circleMarker([34.0522,-118.2437],{
-    radius: 300,
-    color: 'black',
-    fillColor: '#ffffa1'
-}).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -32,3 +17,19 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+let cityData = cities
+//   loop through each city array and create one marker for each city
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: (city.population/100000),
+        color: 'orange',
+        weight: 4
+
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+});
+
+
