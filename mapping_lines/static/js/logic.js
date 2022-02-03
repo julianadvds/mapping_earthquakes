15818da6126490d8c82520d3f3e43da6610ccec8
Juliana dvdS <julianadvds@gmail.com>
2022-02-02 23:25:44 -0500

    completed adding a line

--- mapping_lines/static/js/logic.js
+++ mapping_lines/static/js/logic.js
@@ -2,14 +2,14 @@
 console.log("working");
 
 
-// Create the map object with a centre and zoom level
-let map = L.map('mapid').setView([40.7,-94.5],4);
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([39.8283, -98.5795], 4.4);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/dark-v10',
+    id: 'mapbox/light-v10',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
@@ -18,18 +18,43 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
-let cityData = cities
-//   loop through each city array and create one marker for each city
-cityData.forEach(function(city) {
-    console.log(city)
-    L.circleMarker(city.location, {
-        radius: (city.population/100000),
-        color: 'orange',
-        weight: 4
+//  //Coordinates for each point to be used in the line.
+// let line = [
+//     [33.9416, -118.4085],
+//     [37.6213, -122.3790]
+//   ];
 
-    })
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-    .addTo(map);
-});
+// // Create a polyline using the line coordinates and make the line red
+// L.polyline(line, {
+//     color: 'red'
+// }).addTo(map);
 
+// // Coordinates for each point to be used in the polyline.
+// let line = [
+//     [33.9416, -118.4085],
+//     [37.6213, -122.3790],
+//     [40.7899, -111.9791],
+//     [47.4502, -122.3088]
+//   ];
 
+// L.polyline(line, {
+//     color: 'yellow'
+// }).addTo(map);
+
+/* skill drill - create airline route from SFO, AUS, YYZ, JFK
+blue dashed line, weight 4, opacity 0.5 light map  
+*/
+
+let line = [
+    [37.6213, -122.3790], // SFO
+    [30.1974,-97.6663 ],
+    [43.6777,-79.6248 ], //yyz
+    [40.6413,-73.7781 ] //JFK
+]
+
+L.polyline(line, {
+    color: 'blue',
+    weight: 4,
+    opacity: 0.5,
+    dashArray: '3,7'
+}).addTo(map);
