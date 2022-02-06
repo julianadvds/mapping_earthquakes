66cd7faa03f4619268792bbe402827c6c3ab22cc
Juliana dvdS <julianadvds@gmail.com>
2022-02-05 21:11:07 -0500

    circle colour and popup

--- Earthquakes_past7days/static/js/logicstep3.js
+++ Earthquakes_past7days/static/js/logicstep3.js
@@ -59,7 +59,7 @@ function styleInfo(feature) {
     return {
       opacity: 1,
       fillOpacity: 1,
-      fillColor: "#ffae42",
+      fillColor: getColor(feature.properties.mag),
       color: "#000000",
       radius: getRadius(feature.properties.mag),
       stroke: true,
@@ -67,7 +67,28 @@ function styleInfo(feature) {
     };
   }
 
-// This function determines the radius of the earthquake marker based on its magnitude.
+// This function determines the color of the circle based on the magnitude of the earthquake.
+function getColor(magnitude) {
+    if (magnitude > 5) {
+      return "#ea2c2c";
+    }
+    if (magnitude > 4) {
+      return "#ea822c";
+    }
+    if (magnitude > 3) {
+      return "#ee9c00";
+    }
+    if (magnitude > 2) {
+      return "#eecc00";
+    }
+    if (magnitude > 1) {
+      return "#d4ee00";
+    }
+    return "#98ee00";
+  }
+
+
+  // This function determines the radius of the earthquake marker based on its magnitude.
 // Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
 function getRadius(magnitude) {
     if (magnitude === 0) {
@@ -75,6 +96,7 @@ function getRadius(magnitude) {
     }
     return magnitude * 4;
   }
+
 
 // Creating a GeoJSON layer with the retrieved data.
 L.geoJSON(data, {
@@ -85,7 +107,16 @@ L.geoJSON(data, {
                 console.log(data);
                 return L.circleMarker(latlng);
             },
-            style: styleInfo
+            style: styleInfo,
+
+            //   // We create a popup for each circleMarker to display the magnitude and
+    //  location of the earthquake after the marker has been created and styled.
+            onEachFeature: function(feature, layer) {
+            layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+      }
+
+
+
         }).addTo(map);
     });
 // Then we add our 'graymap' tile layer to the map.
